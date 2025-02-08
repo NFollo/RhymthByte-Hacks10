d2ea540a83decaf835dc2d603991345c9c946422
husky3802 <nfollo7@gmail.com>
2025-02-07 22:10:45 -0500

    Editted title screen

--- Assets/Scripts/BackgroundMove.cs
+++ Assets/Scripts/BackgroundMove.cs
@@ -4,13 +4,14 @@ public class BackgroundMove : MonoBehaviour
 {
     public float maxX;
     public float maxY;
+    public float t;
     // Update is called once per frame
     void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0f;
-        mousePos.x = Mathf.Clamp(mousePos.x * -0.03f, -1f * maxX, maxX);
-        mousePos.y = Mathf.Clamp(mousePos.y * -0.03f, -1f * maxY, maxY);
+        mousePos.x = Mathf.Lerp(this.transform.position.x, Mathf.Clamp(mousePos.x * -0.03f, -1f * maxX, maxX), t * Time.deltaTime);
+        mousePos.y = Mathf.Lerp(this.transform.position.y, Mathf.Clamp(mousePos.y * -0.03f, -1f * maxY, maxY), t * Time.deltaTime);
         this.transform.position = mousePos;
     }
 }

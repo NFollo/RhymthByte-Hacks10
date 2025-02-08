ed879e4bbda5c3bf8d15b0c23f207611d2830e3e
husky3802 <nfollo7@gmail.com>
2025-02-07 22:40:12 -0500

    More Title Screen Animations

--- Assets/Scripts/IntroAnim.cs
+++ Assets/Scripts/IntroAnim.cs
@@ -4,16 +4,14 @@ public class IntroAnim : MonoBehaviour
 {
     public float speed;
     public float delay;
-    public int direction
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    public int direction;
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.position += new Vector3((direction == speed * Time.deltaTime), , 0f);
+        if (delay <= 0) {
+            this.transform.position += new Vector3(direction%2 == 1 ? (direction == 1 ? 1f : -1f) * speed * Time.deltaTime : 0f, direction%2 == 0 ? (direction == 0 ? 1f : -1f) * speed * Time.deltaTime : 0f, 0f);
+        } else {
+            delay -= Time.deltaTime;
+        }
     }
 }
--- Assets/Scripts/IntroCircle.cs
+++ Assets/Scripts/IntroCircle.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class IntroCircle : MonoBehaviour
 {
+    public float speed;
+    public float acceleration;
+    public float delay;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        this.transform.localScale = new Vector3(0f, 0f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (delay <= 0) {
+
+            this.transform.localScale += new Vector3( speed*Time.deltaTime, speed*Time.deltaTime, 1f);
+            speed += acceleration * Time.deltaTime;
+
+        } else {
+            delay -= Time.deltaTime;
+        }
     }
 }
